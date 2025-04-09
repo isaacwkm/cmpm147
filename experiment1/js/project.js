b9a4d608baaff89e676f546a5c984eda9982dee9
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2025-04-09 00:02:55 -0700

    working experiment 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -15,7 +15,7 @@ class MyProjectClass {
     this.property1 = param1;
     this.property2 = param2;
   }
-  
+
   // define a method
   myMethod() {
     // code to run when method is called
@@ -23,12 +23,124 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    adventurer: [
+      "My Nautical Friend",
+      "Pirate",
+      "Sailor",
+      "Sea Adventurer",
+      "Sea Traveller",
+      "Fellow Landlubber",
+      "Citizen of the Sky",
+      "Sea Dog",
+      "$adventurer and $adventurer",
+      "$adventurer, $adventurer, and $adventurer",
+    ],
+    pre: ["Edelweiss", "Circadia", "Moss", "Canyon", "Fischer's"],
+    post: ["", " Island", " Isle", "Lookout"],
+    people: [
+      "kindly",
+      "meek",
+      "brave",
+      "wise",
+      "sacred",
+      "cherished",
+      "honored",
+      "forgotten",
+      "apathetic",
+      "mystic",
+      "orca",
+    ],
+    item: [
+      "axe",
+      "staff",
+      "book",
+      "cloak",
+      "shield",
+      "club",
+      "sword",
+      "magic gloves",
+      "galvel",
+      "fists",
+      "mace",
+      "potato",
+    ],
+    num: [
+      "two",
+      "three",
+      "eleven",
+      "so many",
+      "too many",
+      "an unsatisfying number of",
+      "barely any",
+      "an unspecified amount of",
+      "surely a satisfactory number of",
+    ],
+    looty: [
+      "gleaming",
+      "valuable",
+      "esteemed",
+      "rare",
+      "exalted",
+      "scintillating",
+      "plunderable",
+    ],
+    loots: [
+      "coins",
+      "chalices",
+      "ingots",
+      "gems",
+      "scrolls",
+      "treasure",
+      "booty",
+    ],
+    baddies: [
+      "skeletons",
+      "glubs",
+      "fishmen",
+      "cordungles",
+      "college (of winterhold) professors",
+      "sea dragon",
+      "evil $adventurer",
+    ],
+    message: ["call", "post", "decree", "hearkens", "harkening"],
+  };
 
-  // call a method on the instance
-  myInstance.myMethod();
+  const template = `$adventurer, heed this $message!
+  
+  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+  
+  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  `;
+
+  // STUDENTS: You don't need to edit code below this line.
+
+  const slotPattern = /\$(\w+)/;
+
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+
+    /* global box */
+    $("#box").text(story);
+  }
+
+  /* global clicker */
+  $("#clicker").click(generate);
+
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
